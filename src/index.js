f59b8477b777321e63afab87048bcec2cbbcecd8
oxavibes <bozzistef@gmail.com>
2022-08-18 18:22:48 -0300

    refactor: remove unnecesary if statement

--- src/index.js
+++ src/index.js
@@ -5,19 +5,17 @@ const dotenv = require("dotenv").config();
 
 let db = null;
 const app = express();
-const port = process.env.PORT || 5000;
 
 //Middlewares
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-  console.log("Development mode");
-}
-
-if (process.env.NODE_ENV === "development") {
   db = process.env.DB_LOCAL;
+
+  app.use(morgan("dev"));
+
+  console.log("Development mode");
 } else {
   db = process.env.DB_ATLAS.replace("<password>", process.env.DB_PASSWORD);
 }
@@ -27,9 +25,10 @@ app.use("/api/v1/notes", require("./routes/noteRoutes"));
 
 //Database
 (async () => {
+  const port = process.env.PORT || 5000;
   try {
     await mongoose.connect(db);
-    console.log('Connected to database successfully...');
+    console.log("Connected to database successfully...");
 
     app.listen(port, () => console.log(`App running on port ${port}...`));
   } catch (error) {}
